93865f0a960deb58842d111ad0fa5f11231b2009
lukas <lukaslanga1@gmail.com>
2026-02-25 20:59:13 +0100

    ADD basic frontend structure

---
+++ frontend/rxconfig.py
@@ -0,0 +1,12 @@
+import reflex as rx
+from dotenv import load_dotenv
+import os
+
+
+dotenv_path = os.path.join(os.path.dirname(__file__), "..", ".env")
+load_dotenv(dotenv_path=dotenv_path)
+
+config = rx.Config(
+    app_name="frontendApp",
+    disable_plugins=['reflex.plugins.sitemap.SitemapPlugin'],
+)
