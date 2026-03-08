66972bacc2bd70da0f30f540cb09e2b601db4d19
lukas <lukaslanga1@gmail.com>
2026-03-08 17:45:20 +0100

    FIX weird looks on firefox

--- frontend/frontendApp/frontendApp.py
+++ frontend/frontendApp/frontendApp.py
@@ -1,14 +1,10 @@
 import reflex as rx
-
-from .pages import index, login, register
-
-theme = rx.theme(
-    appearance="light",
-    has_background=True,
-    radius="large",
-    accent_color="grass",
-)
+from .styles import base_style
+from .pages import index, login, register, transaction_detail, transactions, categories
 
 app = rx.App(
-    theme=theme,
+    style=base_style,
+    stylesheets=[
+        "https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap",
+    ],
 )
